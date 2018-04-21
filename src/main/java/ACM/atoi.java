8c2f965a25dd1e70bed30e1f8e2289c4ad8753c5
cumtfc <fc751590330@outlook.com>
2018-04-21 16:24:30 +0800

    update

--- src/main/java/ACM/atoi.java
+++ src/main/java/ACM/atoi.java
@@ -3,27 +3,51 @@ package ACM;
 public class atoi {
     public static void main(String[] args) {
         Solution solution = new Solution();
-        System.out.println(solution.myAtoi("123"));
+        System.out.println(solution.myAtoi("+-2"));
     }
 
     static class Solution {
         public int myAtoi(String str) {
             str = str.trim();
-            char c = str.charAt(0);
-            if (c != '-') {
+            if (str.isEmpty()) {
                 return 0;
             }
-            if (c < 'A' || c > 'z'){
-                return 0;
+            char c = str.charAt(0);
+            boolean flag = false;
+            if (c == '-') {
+                str = str.substring(1);
+                flag = true;
+            }else if(c == '+'){
+                str = str.substring(1);
             }
             int i = 0;
             for (; i < str.length(); i++) {
                 char c1 = str.charAt(i);
-                if (c1 != '-' && (c1 < '0' || c1 > '9')) {
+                if (c1 < '0' || c1 > '9') {
                     break;
                 }
             }
-            return Integer.parseInt(str.substring(0, i));
+            if (i == 0) {
+                return 0;
+            }
+            int anInt = 0;
+            if (flag) {
+                try {
+                    anInt = Integer.parseInt("-" + str.substring(0, i));
+                } catch (Exception e) {
+                    anInt = Integer.MIN_VALUE;
+                }finally {
+                    return anInt;
+                }
+            }else {
+                try {
+                    anInt = Integer.parseInt(str.substring(0, i));
+                } catch (Exception e) {
+                    anInt = Integer.MAX_VALUE;
+                }finally {
+                    return anInt;
+                }
+            }
         }
     }
 }
