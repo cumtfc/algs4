9fb2e633d83a357385dad934772aa0599d41dbc9
fc <fc751590330@outlook.com>
2018-04-26 16:41:04 +0800

    update

---
+++ src/main/java/ACM/RegularExpressionMatching.java
@@ -0,0 +1,27 @@
+package ACM;
+
+public class RegularExpressionMatching {
+    public static void main(String[] args) {
+        Solution solution = new Solution();
+        System.out.println(solution.isMatch("aa", "a"));
+        System.out.println(solution.isMatch("aa", "a*"));
+        System.out.println(solution.isMatch("ab", ".*"));
+        System.out.println(solution.isMatch("aab", "c*a*b"));
+        System.out.println(solution.isMatch("mississippi", "mis*is*p*."));
+    }
+
+    static class Solution {
+        public boolean isMatch(String text, String pattern) {
+            if (pattern.isEmpty()) return text.isEmpty();
+            boolean first_match = (!text.isEmpty() &&
+                    (pattern.charAt(0) == text.charAt(0) || pattern.charAt(0) == '.'));
+
+            if (pattern.length() >= 2 && pattern.charAt(1) == '*'){
+                return (isMatch(text, pattern.substring(2)) ||
+                        (first_match && isMatch(text.substring(1), pattern)));
+            } else {
+                return first_match && isMatch(text.substring(1), pattern.substring(1));
+            }
+        }
+    }
+}
