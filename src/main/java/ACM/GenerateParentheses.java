6d926c9e21fd37de6af4cc6564d2add5aa140035
cumtfc <fc751590330@outlook.com>
2018-05-28 14:58:50 +0800

    update

---
+++ src/main/java/ACM/GenerateParentheses.java
@@ -0,0 +1,31 @@
+package ACM;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class GenerateParentheses {
+    public static void main(String[] args) {
+        Solution solution = new Solution();
+        solution.generateParenthesis(3);
+    }
+
+    static class Solution {
+        private List<String> list;
+
+        public List<String> generateParenthesis(int n) {
+            list = new ArrayList<>();
+            generateParenthesis(n, "", "");
+            return list;
+        }
+
+        public void generateParenthesis(int n, String left, String right) {
+            String str = "(" + left + ")" + right;
+            if (--n == 0) {
+                list.add(str);
+            } else {
+                generateParenthesis(n, str, "");
+                generateParenthesis(n, "", str);
+            }
+        }
+    }
+}
